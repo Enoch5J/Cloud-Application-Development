933ce398bc4312738c5d512a5e5436f446a617bb
Enoch5J <142391930+Enoch5J@users.noreply.github.com>
2023-11-01 19:01:44 +0530

    Add files via upload

---
+++ Personal Blog/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text" ,{ 
+    strings:["Mohammed Sharik","MD Muqbil Sinan","Jai Ganesh","Enoch"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+console.log("Hi")
